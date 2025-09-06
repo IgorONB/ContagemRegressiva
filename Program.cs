45de04bd397c045d4839e9a90b922848e2c2d590
Igor Oscar <igoroscaronb16@gmail.com>
2025-09-05 22:17:54 -0300

    program pronto

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("preparar...");
+Console.ReadKey();
+Console.WriteLine("apontar");
+Console.ReadKey();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("FOGO!");
+Console.ResetColor();
